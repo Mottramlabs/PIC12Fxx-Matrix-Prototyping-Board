dcd749cafdabaf6a36abceac93e57e0a0f41191e
Mottramlabs <8254712+Mottramlabs@users.noreply.github.com>
2020-07-10 16:31:44 +0100

    update

--- PIC12F683 Hello World/Hello_World.asm
+++ PIC12F683 Hello World/Hello_World.asm
@@ -1,84 +1,91 @@
 ;                   *************************************** Title block ***********************************************
-;		Customer:		Mottramlabs
-;		Product:		Hello World
-;		Last update:        16th May 2020
-;		Author:		David Mottram
-;		Device:		PIC12F683
-;		Clock:		16MHz Crystal
-;		---------------------------------------------------------------------------------------------------
-; Revision notes:
+;                   Customer:           Mottramlabs
+;                   Product:            Hello World
+;                   Last update:        16th May 2020
+;                   Author:             David Mottram
+;                   Device:             PIC12F683
+;                   Clock:              Internal 8MHz
+;                   ---------------------------------------------------------------------------------------------------
 
-                    #define		F_CPU               8000000            ;Xtal frequency
+                    #define             F_CPU               8000000             ; Xtal frequency
 
-		;Device Selection
-		include		PIC12F683_Config_Bits.asm               ;Configuration Bits
-		include		PIC12F683_Registers.asm		;Regsiters and bits
+                    ; device Selection
+                    include             PIC12F683_Config_Bits.asm               ; configuration Bits
+                    include             PIC12F683_Registers.asm                 ; regsiters and bits
 
                     ;************************************* General purpose registers 20h - 7Fh ************************
-                    CBLOCK		020h                                    ;Start of Variable space address
-                    Time_1                                                      ;Used by timers
-                    Time_2						;Used by timers
-                    Time_3						;Used by timers
-                    Temp_1						;Temp Reg.
-                    Temp_2						;Temp Reg.
-                    Temp_3						;Temp Reg.
-                    Temp_4						;Temp Reg.
-                    Temp_5						;Temp Reg.
-                    Temp_6						;Temp Reg.
-                    Temp_7						;Temp Reg.													;Temp Reg.
-                    ENDC														;End if C Block
+                    CBLOCK              020h                                    ; start of Variable space address
+                    Time_1                                                      ; used by timers
+                    Time_2                                                      ; used by timers
+                    Time_3                                                      ; used by timers
+                    Temp_1                                                      ; temp reg
+                    Temp_2                                                      ; temp reg
+                    Temp_3                                                      ; temp reg
+                    ENDC                                                        ; end if C Block
 
-                    #define             LED                 5                   ;LED connection
+                    #define             LED                 GP5                 ; LED
                     
-		org		00h                                     ;Reset vector
+                    org                 00h                                     ; reset vector
 
-                    bsf		Status,RP0			;Reg bank select
-		;Set internal osc to 8MHz                                   
-                    movlw		b'01110011'                             ;Load w
-		movwf		OSCCON				;Load reg
-		movlw		b'00000000'			;Load w
-		movwf		ANSEL				;Select all I/O as digital
-                    bcf                 TRISG0				;I/O
-                    bcf                 TRISG1				;I/O
-                    bcf                 TRISG2				;I/O
-                    bcf                 TRISG3				;I/O
-                    bcf                 TRISG4				;I/O
-                    bcf                 TRISG5				;I/O
-                    bcf		Status,RP0			;Reg bank select
+                    ; setup a few things including turning off comparators and interupts    
+                    bsf                 Register_Bank_0                         ; reg bank select
+                    movlw               b'00001111'                             ; watchdog control, timer0 to Watchdog and slowest setting
+                    movwf               OPTION_REG                              ; load option reg
+                    bcf                 IOC3                                    ; interrupt-on-change GPIO Control bits
+                    bcf                 IOC2                                    ; interrupt-on-change GPIO Control bits
+                    bcf                 IOC1                                    ; interrupt-on-change GPIO Control bits
+                    bcf                 IOC0                                    ; interrupt-on-change GPIO Control bits
+                    bcf                 Register_Bank_0                         ; reg bank select
+                    bcf                 GBIE                                    ; RB Port Change Interrupt Enable bit
+                    bsf                 CM0                                     ; comparator off
+                    bsf                 CM1                                     ; comparator off
+                    bsf                 CM2                                     ;Comparator off
+                    
+                    bsf                 Register_Bank_0                         ; select Register Bank 1
+                    bcf                 TRISG5                                  ; port direction
+                    bcf                 TRISG4                                  ; port direction
+                    bcf                 TRISG2                                  ; port direction                
+                    bsf                 TRISG1                                  ; port direction
+                    ;clrf               ANSEL                                   ; turn off A/D, 12F675 only
+                    bcf                 Register_Bank_0                         ; select Register Bank 1
 
-                    ;Define and enable I/O
-                    #Define		PCB_LED             Port_G,LED          ;LED
-                    bcf		PCB_LED                                 ;LED Control
+                    movlw               0x07                                    ; load w
+                    movwf               CMCON0                                  ; comarators off, digital I/O only
+                       
+   ;                 ;Define and enable I/O
+   ;                 #Define        PCB_LED             Port_G,LED          ;LED
+   ;                 bcf        PCB_LED                                 ;LED Control
 
-                    goto                Start				;Jump
+                    bcf                 LED                                     ; LED control
+                    goto                Start                                   ; jump
                     
                     ;--------------------------------------------------------------------------------------------------------------
                     ;*********************************  Start of Sub routines *****************************************************
                     ;--------------------------------------------------------------------------------------------------------------
 
-		include		Timer_SUBS.asm			;Timer sub routines
+                    include             Timer_SUBS.asm                          ; timer sub routines
 
-		;-------------------------------------------------------------------------------------------------
-		;********************************* End of Sub routines *******************************************
-		;-------------------------------------------------------------------------------------------------
+                    ;-------------------------------------------------------------------------------------------------
+                    ;********************************* End of Sub routines *******************************************
+                    ;-------------------------------------------------------------------------------------------------
 
-Start		;Program start
+Start               ; program start
                     
-                    movlw               .004                                    ;Load w, number of flash loops
-                    movwf               Temp_1                                  ;Load reg
-Loop                ;Flash loop
-            	bsf		PCB_LED                                 ;LED Control
-		call                Del_10mS                                ;Delay
-		bcf		PCB_LED                                 ;LED Control
+                    movlw               .004                                    ; load w, number of flash loops
+                    movwf               Temp_1                                  ; load reg
+Loop                ; flash loop
+                    bsf                 LED                                     ; LED control
+                    call                Del_10mS                                ; delay
+                    bcf                 LED                                     ; LED control
                     
-                    call                Del_100mS                               ;Delay
-                    call                Del_100mS                               ;Delay
-                    decfsz              Temp_1,1                                ;Dec counter and skip next if zero
-                    goto                Loop                                    ;Next flash
-                    
-                    call                Del_1S                                  ;Delay
+                    call                Del_100mS                               ; delay
+                    call                Del_100mS                               ; delay
 
-                    goto                Start                                   ;Loop
-		    
+                    decfsz              Temp_1,1                                ; dec counter and skip next if zero
+                    goto                Loop                                    ; next flash
+                    
+                    call                Del_1S                                  ; delay
+                    goto                Start                                   ; loop
+            
 end
-	    
+        
--- PIC12F683 Hello World/PIC12F683_Config_Bits.asm
+++ PIC12F683 Hello World/PIC12F683_Config_Bits.asm
@@ -1,37 +1,30 @@
-
-;=======================================
+; data sheet to p52
+                    
+;================================================
 ;  Configuration (FUSE) BIT Definitions 
-;=======================================
-_FCMEN_ON            EQU        3FFFh       
-_FCMEN_OFF           EQU        37FFh       
-_IESO_ON             EQU        3FFFh       
-_IESO_OFF            EQU        3BFFh       
-_BOD_ON              EQU        3FFFh       
-_BOD_NSLEEP          EQU        3EFFh       
-_BOD_SBODEN          EQU        3DFFh       
-_BOD_OFF             EQU        3CFFh       
-_CPD_ON              EQU        3F7Fh       
-_CPD_OFF             EQU        3FFFh       
-_CP_ON               EQU        3FBFh       
-_CP_OFF              EQU        3FFFh       
-_MCLRE_ON            EQU        3FFFh       
-_MCLRE_OFF           EQU        3FDFh       
-_PWRTE_OFF           EQU        3FFFh       
-_PWRTE_ON            EQU        3FEFh       
-_WDT_ON              EQU        3FFFh       
-_WDT_OFF             EQU        3FF7h       
-_LP_OSC              EQU        3FF8h       
-_XT_OSC              EQU        3FF9h       
-_HS_OSC              EQU        3FFAh       
-_EC_OSC              EQU        3FFBh       
-_INTRC_OSC_NOCLKOUT  EQU        3FFCh       
-_INTOSCIO            EQU        3FFCh       
-_INTRC_OSC_CLKOUT    EQU        3FFDh       
-_INTOSC              EQU        3FFDh       
-_EXTRC_OSC_NOCLKOUT  EQU        3FFEh       
-_EXTRCIO             EQU        3FFEh       
-_EXTRC_OSC_CLKOUT    EQU        3FFFh       
-_EXTRC               EQU        3FFFh       
+;================================================
+_CPD_ON                      EQU     H'3EFF'
+_CPD_OFF                     EQU     H'3FFF'
+_CP_ON                       EQU     H'3F7F'
+_CP_OFF                      EQU     H'3FFF'
+_BODEN_ON                    EQU     H'3FFF'
+_BODEN_OFF                   EQU     H'3FBF'
+_MCLRE_ON                    EQU     H'3FFF'
+_MCLRE_OFF                   EQU     H'3FDF'
+_PWRTE_OFF                   EQU     H'3FFF'
+_PWRTE_ON                    EQU     H'3FEF'
+_WDT_ON                      EQU     H'3FFF'
+_WDT_OFF                     EQU     H'3FF7'
+_LP_OSC                      EQU     H'3FF8'
+_XT_OSC                      EQU     H'3FF9'
+_HS_OSC                      EQU     H'3FFA'
+_EC_OSC                      EQU     H'3FFB'
+_INTRC_OSC_NOCLKOUT          EQU     H'3FFC'
+_INTRC_OSC_CLKOUT            EQU     H'3FFD'
+_EXTRC_OSC_NOCLKOUT          EQU     H'3FFE'
+_EXTRC_OSC_CLKOUT            EQU     H'3FFF'
 
-
-__CONFIG    _MCLRE_ON & _WDT_ON & _INTRC_OSC_NOCLKOUT & _BOD_OFF
+; turning off Brownout detection dropped the sleep current from 110uA to 30uA
+; turning off MCLRE made no difference            
+; turning power up timer made it no difference            
+__CONFIG _CPD_OFF & _CP_OFF & _BODEN_OFF & _MCLRE_OFF & _PWRTE_ON & _WDT_ON  & _INTRC_OSC_NOCLKOUT
